df50be4ab818ce8fbf9786f0791f360e659d4181
regor <regor@DESKTOP-281SH0F>
2020-01-08 08:47:58 -0800

    Extending on Dave's default interface so that move, copy, with or without exclusion of duplicates will be supported.

--- BusinessLogic3/src/main/java/ku/piii2019/bl3/FileService.java
+++ BusinessLogic3/src/main/java/ku/piii2019/bl3/FileService.java
@@ -5,6 +5,7 @@
  */
 package ku.piii2019.bl3;
 
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Set;
 
@@ -13,8 +14,23 @@ import java.util.Set;
  * @author James
  */
 public interface FileService {
-    Set<MediaItem>          getAllMediaItems    (String rootFolder);
-    Set<MediaItem>          getItemsToRemove    (Set<Set<MediaItem>> duplicates);
-    boolean                 removeFiles         (Set<MediaItem> listToRemove);
+    Set<MediaItem>          getAllMediaItems                   (String rootFolder);
+    Set<MediaItem>          getItemsToRemove                   (Set<Set<MediaItem>> duplicates);
+    boolean                 removeFiles                        (Set<MediaItem> listToRemove);
+    
+    
+    Set<Path>               getAllFiles                        (String rootFolder);
+    Set<Path>               getM3UFiles                        (String rootFolder);
+    
+    boolean                 moveMP3Files                        (String srcFolder, String dstFolder);
+    boolean                 moveMP3FilesExcludingDuplicates     (String srcFolder, String dstFolder, DuplicateFinder df);
+    boolean                 copyMP3Files                        (String srcFolder, String dstFolder);
+    boolean                 copyMP3FilesExcludingDuplicates     (String srcFolder, String dstFolder, DuplicateFinder df);
+    
+     boolean                 moveMP3Files                       (List<MediaItem> srcFolder, String dstFolder);
+    boolean                 moveMP3FilesExcludingDuplicates     (List<MediaItem> srcFolder, String dstFolder, DuplicateFinder df);
+    boolean                 copyMP3Files                        (List<MediaItem> srcFolder, String dstFolder);
+    boolean                 copyMP3FilesExcludingDuplicates     (List<MediaItem> srcFolder, String dstFolder, DuplicateFinder df);
+   
     
 }
