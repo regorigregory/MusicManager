9c85b7734bfc3a88e53396d8b6d5603107856f79
regor <regor@DESKTOP-281SH0F>
2020-01-13 22:57:37 -0800

    GUI-7: Now it is really done. Updated MediaItem so that its main attributes can be edited outside of the class. Also, tested if it works in Playground.java.

--- BusinessLogic3/src/main/java/ku/piii2019/bl3/PlayGround.java
+++ BusinessLogic3/src/main/java/ku/piii2019/bl3/PlayGround.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package ku.piii2019.bl3;
+import java.util.Set;
 import org.apache.commons.text.similarity.LevenshteinDistance;
 
 /**
@@ -12,9 +13,10 @@ import org.apache.commons.text.similarity.LevenshteinDistance;
  */
 public class PlayGround {
     public static void main(String... args){
-        
-        
-        
-        
+        String folder = "C:\\gdrive\\NetBeansProjects\\music-manager-assignment\\test_folders\\original_filenames\\collection-B\\After Many Days";
+        Set<MediaItem> test = MediaFileService.getInstance().getAllID3MediaItems(folder, null);
+        MediaItem m = test.iterator().next();
+        m.setArtist("pussy");
+        MediaInfoSourceFromID3.updateBasicMetaTags(m);
     }
 }
