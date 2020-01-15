4de9f683cfb658c8e665b63ebe5ca178eccd76ab
regor <regor@DESKTOP-281SH0F>
2020-01-15 13:25:55 -0800

    DEV-1: Yes. I was right. The issue is with inheritance.
    Therefore: the root logger ("") has to be handled first.
    
    Then can add additional loggers to the child?
    
    Perhaps change the logic so that it can only add FileHandlers to this namespace.

--- BusinessLogic3/src/main/java/ku/piii2019/bl3/CustomLogging.java
+++ BusinessLogic3/src/main/java/ku/piii2019/bl3/CustomLogging.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package ku.piii2019.bl3;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,102 +17,107 @@ import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
+
 /**
  *
  * @author regor
  */
 public class CustomLogging {
-    private static Logger LOGGER_INSTANCE=null;
+
+    private static Logger LOGGER_INSTANCE = null;
     private static Level DEFAULT_LOG_LEVEL = Level.WARNING;
     private static String DEFAULT_LOG_FILE = "./log.log";
-  
-    public static Logger getInstance(){
-        if(LOGGER_INSTANCE == null){
+
+    public static Logger getInstance() {
+        if (LOGGER_INSTANCE == null) {
             LOGGER_INSTANCE = Logger.getLogger(CustomLogging.class.getName());
         }
         return LOGGER_INSTANCE;
     }
-    public static void defaultSetup(){
+
+    public static void defaultSetup() {
+
         CustomLogging.getInstance();
         CustomLogging.getInstance().setLevel(DEFAULT_LOG_LEVEL);
-        CustomLogging.getInstance().addHandler(getConsoleHandler());
+        removeRootLoggerHandlers();
+        Logger.getLogger("").addHandler(getConsoleHandler());
 
     }
-    
-    
+
     public static void logIt(Exception ex) {
         ex.printStackTrace();
     }
 
     public static void logIt(String msg, Level level) {
-        if(level==null){
+        if (level == null) {
             level = CustomLogging.DEFAULT_LOG_LEVEL;
         }
         CustomLogging.getInstance().log(level, msg);
     }
-    
-      public static void printHandlers(){
-        Handler[] handlers =getInstance().getHandlers();
+
+    public static void printHandlers() {
+        Handler[] handlers = getInstance().getHandlers();
         System.out.println(handlers.length);
         Arrays.asList(handlers).stream().forEach(System.out::println);
-     
+
     }
-    
-    public static void setDefaultFilePath(String path){
+
+    public static void setDefaultFilePath(String path) {
         DEFAULT_LOG_FILE = path;
     }
-    
-    public static void setDefaultLoggingLevel(Level l){
+
+    public static void setDefaultLoggingLevel(Level l) {
         CustomLogging.DEFAULT_LOG_LEVEL = l;
     }
-    public static void addHandler(Handler h){
+
+    public static void addHandler(Handler h) {
         CustomLogging.getInstance().addHandler(h);
     }
-    
-    public static void removeHandler(Handler h){
+
+    public static void removeHandler(Handler h) {
         CustomLogging.getInstance().removeHandler(h);
     }
-    
-    public static void setLevel(Level l){
+
+    public static void setLevel(Level l) {
         getInstance().setLevel(l);
     }
-    
-    public static void removeAllHandlers(){
+
+    public static void removeAllHandlers() {
         Handler[] handlers = CustomLogging.getInstance().getHandlers();
-        for(Handler h : handlers){
+        for (Handler h : handlers) {
             CustomLogging.getInstance().removeHandler(h);
         }
     }
-   
-    public static Handler getConsoleHandler(){
+
+    public static Handler getConsoleHandler() {
         ConsoleHandler ch = new ConsoleHandler();
         SimpleFormatter sf = new SimpleFormatter();
         ch.setFormatter(sf);
         return ch;
     }
-    
-    public static Handler getFileHandler(String fileName){
+
+    public static Handler getFileHandler(String fileName) {
         String pathString = DEFAULT_LOG_FILE;
         SimpleFormatter sf = new SimpleFormatter();
-        if(fileName!=null){
+        if (fileName != null) {
             pathString = fileName;
         }
-        try{
+        try {
             Path candidateFilePath = Paths.get(fileName);
             Path candidateFolder = candidateFilePath.getParent();
-            
-            if(!Files.isDirectory(candidateFolder)){
+
+            if (!Files.isDirectory(candidateFolder)) {
                 Files.createDirectories(candidateFolder);
             }
-            if(!Files.exists(candidateFilePath)){
+            if (!Files.exists(candidateFilePath)) {
                 Files.createFile(candidateFilePath);
             }
-            
-            Handler h =new FileHandler(pathString);
+
+            Handler h = new FileHandler(pathString);
             h.setEncoding("UTF-8");
             h.setFormatter(sf);
-        return h;
-        }catch (Exception e){
+            return h;
+        } catch (Exception e) {
             System.out.println("There has been an error when trying to create the log file.");
             e.printStackTrace();
         }
@@ -121,6 +127,22 @@ public class CustomLogging {
     public static Level getDEFAULT_LOG_LEVEL() {
         return DEFAULT_LOG_LEVEL;
     }
-    
-    
+
+    public static Logger removeRootLoggerHandlers() {
+        /* This code has been unashamedly inspired by:
+        https://www.vogella.com/tutorials/Logging/article.html
+         */
+
+        Logger rootLogger = Logger.getLogger("");
+        Handler[] handlers = rootLogger.getHandlers();
+
+        /*inspirational part over*/
+        for (Handler h : handlers) {
+
+            rootLogger.removeHandler(h);
+
+        }
+        return rootLogger;
+
+    }
 }
