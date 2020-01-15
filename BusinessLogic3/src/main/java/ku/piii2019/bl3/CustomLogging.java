ce505830ac1ff8550217525c8e66c7851426cf52
regor <regor@DESKTOP-281SH0F>
2020-01-15 07:59:01 -0800

    DEV-1: initial commit. No business logic has been implemented.

--- BusinessLogic3/src/main/java/ku/piii2019/bl3/CustomLogging.java
+++ BusinessLogic3/src/main/java/ku/piii2019/bl3/CustomLogging.java
@@ -4,13 +4,20 @@
  * and open the template in the editor.
  */
 package ku.piii2019.bl3;
-
+import java.util.logging.Logger;
 /**
  *
  * @author regor
  */
-public interface CustomLogging {
-
+public class CustomLogging {
+    private static Logger LOGGER_INSTANCE=null;
+    
+    public static Logger getLoggerInstance(){
+        if(LOGGER_INSTANCE == null){
+            LOGGER_INSTANCE = Logger.getLogger(CustomLogging.class.getName());
+        }
+        return LOGGER_INSTANCE;
+    }
     public static void logIt(Exception ex) {
         ex.printStackTrace();
     }
